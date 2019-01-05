1f57669c51ce26ee820e22f49d38fc56f348e693
KamilKluba <226016@student.pwr.edu.pl>
2019-01-05 03:15:31 +0100

    Fixed some bugs

--- src/main/java/studia/pwr/semestr5/WypozyczalniaSprzetuSportowego/app/ManageAssortment.java
+++ src/main/java/studia/pwr/semestr5/WypozyczalniaSprzetuSportowego/app/ManageAssortment.java
@@ -5,26 +5,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.regex.Pattern;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import studia.pwr.semestr5.WypozyczalniaSprzetuSportowego.database.Address;
 import studia.pwr.semestr5.WypozyczalniaSprzetuSportowego.database.Assortment;
-import studia.pwr.semestr5.WypozyczalniaSprzetuSportowego.database.Client;
-import studia.pwr.semestr5.WypozyczalniaSprzetuSportowego.database.Model;
-import studia.pwr.semestr5.WypozyczalniaSprzetuSportowego.database.Person;
 
 public class ManageAssortment {
-	
+
 	private ArrayList<Assortment> arrayListAssortment;
 	private boolean create;
 	private Assortment assortment;
@@ -32,7 +27,7 @@ public class ManageAssortment {
 	JPanel panelDBContent;
 
 	JDialog dialogManageAssortment;
-	
+
 	JLabel labelAssortmentID;
 	JTextField textFieldAssortmentID;
 	JLabel labelModelID;
@@ -44,38 +39,37 @@ public class ManageAssortment {
 	JLabel labelLastRentDate;
 	JTextField textFieldLastRentDate;
 	JLabel LabelAvailability;
-	JTextField textFieldAvailability;
-	JLabel labelNumberOfMaintenance;
-	JTextField textFieldNumberOfMaintenance;
+	JCheckBox checkBoxAvailability;
 	JLabel labelDateNextMaintenance;
 	JTextField textFieldDateNextMaintenance;
 	JLabel labelCondition;
 	JTextField textFieldCondition;
-	JLabel labelInCirculation;
-	JTextField textFieldInCirculation;
+	JLabel labelNumberOrdersNumber;
+	JComboBox<Integer> comboBoxOrdersNumber;
 	JLabel labelMaintenanceNumber;
-	JTextField textFieldMaintenanceNumber;
+	JComboBox<Integer> comboBoxMaintenanceNumber;
 	JLabel labelRepairNumber;
-	JTextField textFieldRepairNumber;
+	JComboBox<Integer> comboBoxRepairNumber;
 	JButton buttonCancel;
 	JButton buttonCreateAssortment;
-	
+
 	public ManageAssortment() {
 	}
 
 	public ManageAssortment(ArrayList<Assortment> arrayListAssortment, boolean create) {
-		
+
 		this.arrayListAssortment = arrayListAssortment;
 		this.create = create;
 
 		initComponents();
 		initListeners();
-		
+
 		dialogManageAssortment.setVisible(true);
 	}
-	
-	public ManageAssortment(ArrayList<Assortment> arrayListAssortment, boolean create, Assortment assortment, JPanel panelDBContent) {
-		
+
+	public ManageAssortment(ArrayList<Assortment> arrayListAssortment, boolean create, Assortment assortment,
+			JPanel panelDBContent) {
+
 		this.arrayListAssortment = arrayListAssortment;
 		this.create = create;
 		this.assortment = assortment;
@@ -85,21 +79,14 @@ public class ManageAssortment {
 		initComponents();
 		initListeners();
 
-		
-		
 		dialogManageAssortment.setVisible(true);
 	}
-	
-	
-	private void divideModel()
-	{
-	
+
+	private void divideModel() {
+
 		dividedModel = assortment.toString().split(Pattern.quote(" !&#*&% "));
 	}
-	
-	
-	
-	
+
 	private void initComponents() {
 		dialogManageAssortment = new JDialog();
 		dialogManageAssortment.setTitle("Dodanie sprzętu");
@@ -107,8 +94,10 @@ public class ManageAssortment {
 		dialogManageAssortment.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		dialogManageAssortment.setResizable(false);
 		dialogManageAssortment.setModal(true);
+		if (!create)
+			dialogManageAssortment.setTitle("Edycja sprzętu");
 		dialogManageAssortment.setLayout(null);
-		
+
 		labelAssortmentID = new JLabel("ID sprzetu:");
 		labelAssortmentID.setBounds(20, 30, 180, 30);
 		dialogManageAssortment.add(labelAssortmentID);
@@ -119,140 +108,125 @@ public class ManageAssortment {
 			textFieldAssortmentID.setEnabled(false);
 		}
 		dialogManageAssortment.add(textFieldAssortmentID);
-		
+
 		labelModelID = new JLabel("ID modelu:");
 		labelModelID.setBounds(20, 70, 180, 30);
 		dialogManageAssortment.add(labelModelID);
 		textFieldModelID = new JTextField();
 		textFieldModelID.setBounds(200, 70, 150, 30);
 		if (!create) {
-			textFieldModelID.setText(dividedModel[1]);
+			textFieldModelID.setText(dividedModel[7]);
 			textFieldModelID.setEnabled(false);
 		}
 		dialogManageAssortment.add(textFieldModelID);
-		
+
 		labelBuyDate = new JLabel("Data zakupu:");
 		labelBuyDate.setBounds(20, 110, 180, 30);
 		dialogManageAssortment.add(labelBuyDate);
 		textFieldBuyDate = new JTextField();
 		textFieldBuyDate.setBounds(200, 110, 150, 30);
 		if (!create)
-			textFieldBuyDate.setText(dividedModel[2]);
+			textFieldBuyDate.setText(dividedModel[1]);
 		dialogManageAssortment.add(textFieldBuyDate);
-		
+
 		labelRentNumber = new JLabel("Liczba wypozyczen:");
 		labelRentNumber.setBounds(20, 150, 180, 30);
 		dialogManageAssortment.add(labelRentNumber);
 		textFieldRentNumber = new JTextField();
 		textFieldRentNumber.setBounds(200, 150, 150, 30);
 		if (!create) {
-			textFieldRentNumber.setText(dividedModel[3]);
+			textFieldRentNumber.setText(dividedModel[2]);
 			textFieldRentNumber.setEnabled(false);
 		}
 		dialogManageAssortment.add(textFieldRentNumber);
-		
+
 		labelLastRentDate = new JLabel("Data ostatniego wypozyczenia:");
 		labelLastRentDate.setBounds(20, 190, 180, 30);
 		dialogManageAssortment.add(labelLastRentDate);
 		textFieldLastRentDate = new JTextField();
 		textFieldLastRentDate.setBounds(200, 190, 150, 30);
 		if (!create) {
-			textFieldLastRentDate.setText(dividedModel[4]);
+			textFieldLastRentDate.setText(dividedModel[3]);
 			textFieldLastRentDate.setEnabled(false);
 		}
 		dialogManageAssortment.add(textFieldLastRentDate);
-		
+
 		LabelAvailability = new JLabel("Dostepnosc:");
 		LabelAvailability.setBounds(20, 230, 180, 30);
 		dialogManageAssortment.add(LabelAvailability);
-		textFieldAvailability = new JTextField();
-		textFieldAvailability.setBounds(200, 230, 150, 30);
-		if (!create)
-			textFieldAvailability.setText(dividedModel[5]);
-		dialogManageAssortment.add(textFieldAvailability);
-		
-		labelNumberOfMaintenance = new JLabel("Liczba konserwacji:");
-		labelNumberOfMaintenance.setBounds(20, 270, 180, 30);
-		dialogManageAssortment.add(labelNumberOfMaintenance);
-		textFieldNumberOfMaintenance = new JTextField();
-		textFieldNumberOfMaintenance.setBounds(200, 270, 150, 30);
-		if (!create) {
-			textFieldNumberOfMaintenance.setText(dividedModel[6]);
-			textFieldNumberOfMaintenance.setEnabled(false);
-		}
-		dialogManageAssortment.add(textFieldNumberOfMaintenance);
-		
+		checkBoxAvailability = new JCheckBox();
+		checkBoxAvailability.setBounds(200, 230, 150, 30);
+		if (!create && dividedModel[4].equals("true"))
+			checkBoxAvailability.setSelected(true);
+		dialogManageAssortment.add(checkBoxAvailability);
+
 		labelDateNextMaintenance = new JLabel("Data nastepnej konserwacji:");
 		labelDateNextMaintenance.setBounds(20, 310, 180, 30);
 		dialogManageAssortment.add(labelDateNextMaintenance);
 		textFieldDateNextMaintenance = new JTextField();
 		textFieldDateNextMaintenance.setBounds(200, 310, 150, 30);
 		if (!create)
-			textFieldDateNextMaintenance.setText(dividedModel[7]);
+			textFieldDateNextMaintenance.setText(dividedModel[5]);
 		dialogManageAssortment.add(textFieldDateNextMaintenance);
-		
+
 		labelCondition = new JLabel("Stan:");
-		labelCondition.setBounds(370, 30, 150, 30);
+		labelCondition.setBounds(20, 270, 150, 30);
 		dialogManageAssortment.add(labelCondition);
 		textFieldCondition = new JTextField();
-		textFieldCondition.setBounds(500, 30, 150, 30);
+		textFieldCondition.setBounds(200, 270, 150, 30);
 		if (!create)
-			textFieldCondition.setText(dividedModel[8]);
+			textFieldCondition.setText(dividedModel[6]);
 		dialogManageAssortment.add(textFieldCondition);
-		
-		labelInCirculation = new JLabel("W obiegu:");
-		labelInCirculation.setBounds(370, 70, 150, 30);
-		dialogManageAssortment.add(labelInCirculation);
-		textFieldInCirculation = new JTextField();
-		textFieldInCirculation.setBounds(500, 70, 150, 30);
-		if (!create) {
-			textFieldInCirculation.setText(dividedModel[9]);
-			textFieldInCirculation.setEnabled(false);
-		}
-		dialogManageAssortment.add(textFieldInCirculation);
-		
-		labelMaintenanceNumber = new JLabel("Numer konserwacji:");
-		labelMaintenanceNumber.setBounds(370, 110, 150, 30);
-		dialogManageAssortment.add(labelMaintenanceNumber);
-		textFieldMaintenanceNumber = new JTextField();
-		textFieldMaintenanceNumber.setBounds(500, 110, 150, 30);
-		if (!create) {
-			textFieldMaintenanceNumber.setText(dividedModel[10]);
-			textFieldMaintenanceNumber.setEnabled(false);
-		}
-		dialogManageAssortment.add(textFieldMaintenanceNumber);
-		
-		labelRepairNumber = new JLabel("Numer naprawy:");
-		labelRepairNumber.setBounds(370, 150, 150, 30);
-		dialogManageAssortment.add(labelRepairNumber);
-		textFieldRepairNumber = new JTextField();
-		textFieldRepairNumber.setBounds(500, 150, 150, 30);
-		if (!create) {
-			textFieldRepairNumber.setText(dividedModel[11]);
-			textFieldRepairNumber.setEnabled(false);
-		}
-		dialogManageAssortment.add(textFieldRepairNumber);
-		
+
 		buttonCancel = new JButton("Anuluj");
 		buttonCancel.setBounds(500, 310, 80, 30);
 		dialogManageAssortment.add(buttonCancel);
 
 		buttonCreateAssortment = new JButton("Utworz");
 		buttonCreateAssortment.setBounds(410, 310, 80, 30);
+		if (!create)
+			buttonCreateAssortment.setText("Zapisz");
 		dialogManageAssortment.add(buttonCreateAssortment);
-		
-		
+
+		labelNumberOrdersNumber = new JLabel("Lista zamowien: ");
+		labelNumberOrdersNumber.setBounds(380, 20, 150, 30);
+		dialogManageAssortment.add(labelNumberOrdersNumber);
+		comboBoxOrdersNumber = new JComboBox<Integer>();
+		comboBoxOrdersNumber.setBounds(560, 20, 150, 30);
+		if (!create)
+			for (Integer i : assortment.getListOrdersNumber())
+				comboBoxOrdersNumber.addItem(i);
+		dialogManageAssortment.add(comboBoxOrdersNumber);
+
+		labelMaintenanceNumber = new JLabel("Lista konserwacji: ");
+		labelMaintenanceNumber.setBounds(380, 60, 150, 30);
+		dialogManageAssortment.add(labelMaintenanceNumber);
+		comboBoxMaintenanceNumber = new JComboBox<Integer>();
+		comboBoxMaintenanceNumber.setBounds(560, 60, 150, 30);
+		if (!create)
+			for (Integer i : assortment.getListMaintenancesNumber())
+				comboBoxOrdersNumber.addItem(i);
+		dialogManageAssortment.add(comboBoxMaintenanceNumber);
+
+		labelRepairNumber = new JLabel("Lista napraw: ");
+		labelRepairNumber.setBounds(380, 100, 150, 30);
+		dialogManageAssortment.add(labelRepairNumber);
+		comboBoxRepairNumber = new JComboBox<Integer>();
+		comboBoxRepairNumber.setBounds(560, 100, 150, 30);
+		if (!create)
+			for (Integer i : assortment.getListRepairsNumber())
+				comboBoxOrdersNumber.addItem(i);
+		dialogManageAssortment.add(comboBoxRepairNumber);
 	}
-	
-private void initListeners(){
-		
-		
+
+	private void initListeners() {
+
 		buttonCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dialogManageAssortment.dispose();
 			}
 		});
-		
+
 		buttonCreateAssortment.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (create)
@@ -261,14 +235,12 @@ private void initListeners(){
 					editAssortment();
 			}
 
-			
 		});
-}
+	}
 
-private void createAssortment() {
-	
-		
-		String assortmentID = textFieldAssortmentID.getText();			
+	private void createAssortment() {
+
+		String assortmentID = textFieldAssortmentID.getText();
 		String modelID = textFieldModelID.getText();
 		java.util.Date buyDate = null;
 		try {
@@ -276,15 +248,14 @@ private void createAssortment() {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		String rentNumber = textFieldRentNumber.getText();		
+		String rentNumber = textFieldRentNumber.getText();
 		java.util.Date lastRentDate = null;
 		try {
 			lastRentDate = new SimpleDateFormat("dd/MM/yyyy").parse(textFieldLastRentDate.getText());
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		String availability  = textFieldAvailability.getText();
-		String numberOfMaintenance = textFieldNumberOfMaintenance.getText();
+		boolean availability = checkBoxAvailability.isSelected();
 		java.util.Date dateNextMaintenance = null;
 		try {
 			dateNextMaintenance = new SimpleDateFormat("dd/MM/yyyy").parse(textFieldDateNextMaintenance.getText());
@@ -292,97 +263,78 @@ private void createAssortment() {
 			ex.printStackTrace();
 		}
 		String condition = textFieldCondition.getText();
-		String circulation = textFieldInCirculation.getText();
-		String maintenanceNumber  = textFieldMaintenanceNumber.getText();
-		String repairNumber  = textFieldRepairNumber.getText();
-		
-		
+
 		if (assortmentID.equals(""))
-		JOptionPane.showMessageDialog(dialogManageAssortment, "Pole ID sprzetu nie moze byc puste"); 
+			JOptionPane.showMessageDialog(dialogManageAssortment, "Pole ID sprzetu nie moze byc puste");
 		else if (modelID.equals(""))
 			JOptionPane.showMessageDialog(dialogManageAssortment, "Pole ID modelu nie moze byc puste");
-		else if (availability.equals(""))
-			JOptionPane.showMessageDialog(dialogManageAssortment, "Pole dostepnosc nie moze byc puste");
 		else if (condition.equals(""))
 			JOptionPane.showMessageDialog(dialogManageAssortment, "Pole stan nie moze byc puste");
 		else if (buyDate == null)
 			JOptionPane.showMessageDialog(dialogManageAssortment, "Wprowadz date zakupu");
-	 	
+
 		else {
-			for(Assortment a : arrayListAssortment)
-				if(Integer.toString(a.getItemID()).equals(assortmentID)){
-					JOptionPane.showMessageDialog(dialogManageAssortment, "Sprzet o podanym ID znajduje sie juz w bazie");
+			for (Assortment a : arrayListAssortment)
+				if (Integer.toString(a.getItemID()).equals(assortmentID)) {
+					JOptionPane.showMessageDialog(dialogManageAssortment,
+							"Sprzet o podanym ID znajduje sie juz w bazie");
 					return;
 				}
 		}
-		int modelID2 =Integer.parseInt(textFieldModelID.getText());
-		int rentNumber2 =Integer.parseInt(textFieldRentNumber.getText());
-		int assortmentID2 =Integer.parseInt(textFieldAssortmentID.getText());
-		
-		arrayListAssortment.add(new Assortment(assortmentID2,
-				 buyDate, rentNumber2, lastRentDate,true, dateNextMaintenance, condition,modelID2 ));
+		int modelID2 = Integer.parseInt(textFieldModelID.getText());
+		int rentNumber2 = Integer.parseInt(textFieldRentNumber.getText());
+		int assortmentID2 = Integer.parseInt(textFieldAssortmentID.getText());
+
+		arrayListAssortment.add(new Assortment(assortmentID2, buyDate, rentNumber2, lastRentDate, true,
+				dateNextMaintenance, condition, modelID2));
 		JOptionPane.showMessageDialog(dialogManageAssortment, "Dodano sprzet");
 		dialogManageAssortment.dispose();
 	}
 
+	private void editAssortment() {
+		String assortmentID = textFieldAssortmentID.getText();
+		String modelID = textFieldModelID.getText();
+		String rentNumber = textFieldRentNumber.getText();
+		boolean availability = checkBoxAvailability.isSelected();
+		String condition = textFieldCondition.getText();
+		java.util.Date dateNextMaintenance = null;
+		try {
+			dateNextMaintenance = new SimpleDateFormat("dd/MM/yyyy").parse(textFieldDateNextMaintenance.getText());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		java.util.Date lastRentDate = null;
+		try {
+			lastRentDate = new SimpleDateFormat("dd/MM/yyyy").parse(textFieldLastRentDate.getText());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		java.util.Date buyDate = null;
+		try {
+			buyDate = new SimpleDateFormat("dd/MM/yyyy").parse(textFieldBuyDate.getText());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-private void editAssortment()
-{
-	String assortmentID = textFieldAssortmentID.getText();			
-	String modelID = textFieldModelID.getText();
-	String rentNumber = textFieldRentNumber.getText();
-	String availability  = textFieldAvailability.getText();
-	String numberOfMaintenance = textFieldNumberOfMaintenance.getText();
-	String condition = textFieldCondition.getText();
-	String circulation = textFieldInCirculation.getText();
-	String maintenanceNumber  = textFieldMaintenanceNumber.getText();
-	String repairNumber  = textFieldRepairNumber.getText();
-	java.util.Date dateNextMaintenance = null;
-	try {
-		dateNextMaintenance = new SimpleDateFormat("dd/MM/yyyy").parse(textFieldDateNextMaintenance.getText());
-	} catch (Exception ex) {
-		ex.printStackTrace();
-	}
-	java.util.Date lastRentDate = null;
-	try {
-		lastRentDate = new SimpleDateFormat("dd/MM/yyyy").parse(textFieldLastRentDate.getText());
-	} catch (Exception ex) {
-		ex.printStackTrace();
-	}
-	java.util.Date buyDate = null;
-	try {
-		buyDate = new SimpleDateFormat("dd/MM/yyyy").parse(textFieldBuyDate.getText());
-	} catch (Exception ex) {
-		ex.printStackTrace();
-	}
-
-	if (assortmentID.equals(""))
-		JOptionPane.showMessageDialog(dialogManageAssortment, "Pole ID sprzetu nie moze byc puste"); 
+		if (assortmentID.equals(""))
+			JOptionPane.showMessageDialog(dialogManageAssortment, "Pole ID sprzetu nie moze byc puste");
 		else if (modelID.equals(""))
 			JOptionPane.showMessageDialog(dialogManageAssortment, "Pole ID modelu nie moze byc puste");
-		else if (availability.equals(""))
-			JOptionPane.showMessageDialog(dialogManageAssortment, "Pole dostepnosc nie moze byc puste");
 		else if (condition.equals(""))
 			JOptionPane.showMessageDialog(dialogManageAssortment, "Pole stan nie moze byc puste");
 		else if (buyDate == null)
 			JOptionPane.showMessageDialog(dialogManageAssortment, "Wprowadz date zakupu");
-	 	
-	else
-		try {
-			
-			assortment.setPucharseDate(buyDate);
-			assortment.setNextMaintenanceDate(dateNextMaintenance);
-			assortment.setCondition(condition);
-			dialogManageAssortment.dispose();
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(dialogManageAssortment, "Pole z ceną lub kaucją zawiera błędne dane!");
-		}
-	
-	
-}
 
+		else
+			try {
+				assortment.setPucharseDate(buyDate);
+				assortment.setNextMaintenanceDate(dateNextMaintenance);
+				assortment.setCondition(condition);
+				dialogManageAssortment.dispose();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(dialogManageAssortment, "Pole z ceną lub kaucją zawiera błędne dane!");
+			}
+
+	}
 
 }
-		
-	
-
